f5394d7cd468451d09ee5125124aab0485af6f23
Cristianx10 <42477569+Cristianx10@users.noreply.github.com>
2020-04-08 16:48:39 -0500

    Scripts

---
+++ Assets/Scripts/CheckGrounded.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGrounded : MonoBehaviour
+{
+    
+    private PlayerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponentInParent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "ground")
+        {
+            player.isGrounded = true;
+            player.jump = false;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "ground")
+        {
+            player.isGrounded = false;
+        }
+    }
+
+    
+    void OnBecameInvisible(){
+        player.transform.position = new Vector3(0,0,0);
+    }
+}
