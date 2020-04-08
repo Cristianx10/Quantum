e02bbb888868af806347195acde06d702c255865
Cristianx10 <42477569+Cristianx10@users.noreply.github.com>
2020-04-07 20:51:43 -0500

    Movimiento y creaction del personaje Basico
    Se dan las dinamicas de movimiento de izquierda y deracha, sallto y se agrego funcionalidades relacionadas con fisicas como la gravedad

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    private bool isGrounded;
+    public float speed = 24f;
+    public float maxSpeed = 6f;
+    bool jump = false;
+    public float jumpForce = 7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+        anim.SetBool("Grounded", isGrounded);
+
+        if(Input.GetKeyUp(KeyCode.UpArrow) && isGrounded){
+            jump = true;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        rb.AddForce(Vector2.right * speed * h);
+
+        float limitedSpeed = Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed);
+        rb.velocity = new Vector2(limitedSpeed, rb.velocity.y);
+
+        if (h > 0.1f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+
+        if (h < -0.1f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
+        if(jump){
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+            jump = false;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "ground")
+        {
+            isGrounded = false;
+        }
+    }
+}
