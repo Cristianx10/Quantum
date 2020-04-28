f3105f4a465bed33f3fceb418cf316f19c77e3da
Cristianx10 <42477569+Cristianx10@users.noreply.github.com>
2020-04-28 15:13:19 -0500

    Install Photon

---
+++ Assets/Scripts/PhotonManagerConexion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Photon.Pun.UtilityScripts
+{
+    public class PhotonManagerConexion : MonoBehaviourPunCallbacks
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            PhotonNetwork.ConnectUsingSettings();
+            //Photon.ConnectUsingSettings();
+        }
+
+        // ...
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("OnConnectedToMaster() was called by PUN.");
+            PhotonNetwork.JoinRandomRoom();
+        }
+        // ...
+
+
+    }
+
+}
