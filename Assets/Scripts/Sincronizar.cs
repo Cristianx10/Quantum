7ff37b152d0c8e31e8c36b3dd7bd0d514403b34f
Cristianx10 <42477569+Cristianx10@users.noreply.github.com>
2020-04-29 15:17:04 -0500

    Rigibody

--- Assets/Scripts/Sincronizar.cs
+++ Assets/Scripts/Sincronizar.cs
@@ -6,15 +6,30 @@ using Photon.Pun;
 public class Sincronizar : MonoBehaviourPun, IPunObservable
 {
 
-    public Vector3 RealPosition;
-    public Quaternion RealRotation;
+    public Vector3 RealPosition = Vector3.zero;
+    public Quaternion RealRotation = Quaternion.identity;
+
+
+    public Vector3 RealPosition1 = Vector3.zero;
+    public Quaternion RealRotation1 = Quaternion.identity;
+
+    public Vector3 RealVelocity1 = Vector3.zero;
+    public Vector3 RealVelocity2 = Vector3.zero;
+
     public Vector3 RealScale;
 
     public Animator anim;
 
+    public Rigidbody2D rb;
+
     void FixedUpdate()
     {
-        Update();
+        if (photonView.IsMine)
+        {
+            rb.position = Vector3.Lerp(rb.position, RealPosition1, 0.4f);
+            rb.velocity = Vector3.Lerp(rb.velocity, RealVelocity1, 0.4f);
+
+        }
     }
 
     // Update is called once per frame
@@ -36,21 +51,27 @@ public class Sincronizar : MonoBehaviourPun, IPunObservable
         {
             stream.SendNext(transform.position);
             stream.SendNext(transform.rotation);
-           // stream.SendNext(transform.localScale);
+            // stream.SendNext(transform.localScale);
 
             stream.SendNext(anim.GetBool("Grounded"));
             stream.SendNext(anim.GetBool("Move"));
             stream.SendNext(anim.GetFloat("Speed"));
+
+            stream.SendNext(rb.position);
+            stream.SendNext(rb.velocity);
         }
         else
         {
             RealPosition = (Vector3)stream.ReceiveNext();
             RealRotation = (Quaternion)stream.ReceiveNext();
-          //  RealScale = (Vector3)stream.ReceiveNext();
+            //  RealScale = (Vector3)stream.ReceiveNext();
 
             anim.SetBool("Grounded", (bool)stream.ReceiveNext());
             anim.SetBool("Move", (bool)stream.ReceiveNext());
             anim.SetFloat("Speed", (float)stream.ReceiveNext());
+
+            RealPosition1 = (Vector3)stream.ReceiveNext();
+            RealVelocity1 = (Vector3)stream.ReceiveNext();
         }
     }
 
