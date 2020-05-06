f31022c29f167ebd0889834610f8e8fc550d1e0d
Cristianx10 <42477569+Cristianx10@users.noreply.github.com>
2020-05-06 15:39:57 -0500

    Carga

--- Assets/Scripts/PlayerInterfacePower.cs
+++ Assets/Scripts/PlayerInterfacePower.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class PlayerInterfacePower : MonoBehaviour
 {
     PlayerController player;
-    Material mPositivo;
-    Material mNegativo;
+    public Material mPositivo;
+    public Material mNegativo;
 
-    GameObject Object;
+    public GameObject medidor;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponentInParent<PlayerController>();
+      
     }
 
     // Update is called once per frame
@@ -22,6 +23,7 @@ public class PlayerInterfacePower : MonoBehaviour
         if(player.power > 0){
            // gameObject.rende
         }
+
         float scalePower = map(player.power, -1, 1, -1, 1);
         transform.localScale = new Vector3(
             scalePower,
