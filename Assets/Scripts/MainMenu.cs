ed136850fc448f55c5bccdb948453c1ab7fb4908
AndresQA <andresquintero199912@gmail.com>
2020-06-03 13:59:18 -0500

    update

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,9 +6,14 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 public void PlayGame(){
-SceneManager.LoadScene(1);
+SceneManager.LoadScene("Inslevel1");
 }
 public void QuitGame(){
     Application.Quit();
 }
+
+public void Nextinslevel1(){
+SceneManager.LoadScene("Nivel");
+}
+
 }
